bb7ca3970696d066d0159400c35261e252f38a19
Rene Zbasnik <zbasnikr@coventry.ac.uk>
2022-04-09 20:44:41 +0000

    Making gherkin files, pupeteer files, fixing some bugs and trying git hooks

--- modules/output.js
+++ modules/output.js
@@ -7,7 +7,7 @@ import { db } from './db.js'
  * @returns {object} name of the forum, avatar, summary and time added
  */
 export async function showForums(){
-    let sql = `SELECT nameOfForum, avatar, summary, added, IDForum FROM newForum;`
+    let sql = `SELECT nameOfForum, avatar, summary, added FROM newForum;`
     let result = await db.query(sql)
     for(let i in result){
         let date = new Date(result[i].added)
@@ -18,6 +18,10 @@ export async function showForums(){
     return result
 }
 
+/**
+ * returns name of the forum, avatar, summary, time added in the database, username, id of the forum and description
+ * @returns {object} name of the forum, avatar, summary,time added, username, id of the forum and description
+ */
 export async function detailForum(id){
     let sql = `SELECT nameOfForum, avatar, summary, added, IDForum, username, description FROM newForum WHERE IDForum = ${id};`
     let result = await db.query(sql)
